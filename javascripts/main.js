0188d5d49210e6861b2eb1c62d1f70e9b9ee698d
qschu <qschu@qq.com>
2015-12-29 00:49:35 +0800

    add

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,18 @@
-console.log('This would be the main JS file.');
+$(function(){
+	var TimeFn = null;
+	var articalId = null;
+    $('.anchor').click(function () {
+        clearTimeout(TimeFn);
+        articalId = $(this).attr("id");
+        TimeFn = setTimeout(function(){
+            var pdf = "<embed id='articleView' width='100%'' height='600' src='articals/"+articalId+".pdf'> </embed> "
+            $("#articalContainer").html(pdf);
+        },300);
+    });
+    
+    $('.anchor').dblclick(function () {
+        clearTimeout(TimeFn);
+        articalId = null;
+        $("#articalContainer").html("");
+    })
+})
