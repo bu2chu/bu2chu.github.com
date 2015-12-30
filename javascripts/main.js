8f1306646fb84a8346d96a8a17ef06cbac40b257
qschu <qschu@qq.com>
2015-12-31 00:50:50 +0800

    add

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,18 +1,24 @@
 $(function(){
-	var TimeFn = null;
-	var articalId = null;
+
     $('.anchor').click(function () {
-        clearTimeout(TimeFn);
-        articalId = $(this).attr("id");
-        TimeFn = setTimeout(function(){
-            var pdf = "<embed id='articleView' width='100%'' height='600' src='articals/"+articalId+".pdf'> </embed> "
-            $("#articalContainer").html(pdf);
-        },300);
+        var articalId = $(this).attr("id");
+        var pdf = "<embed id='articleView' width='100%'' height='100%' src='articals/"+articalId+".pdf'> </embed> "
+        $("#articalMask").show();
+        $("#articalContainer").html(pdf);
+        $("#articalContainer").show();
+        $('#optHint').show();
     });
     
-    $('.anchor').dblclick(function () {
-        clearTimeout(TimeFn);
-        articalId = null;
+    $('#articalMask').dblclick(function () {
+        $("#articalMask").hide();
+        $("#articalContainer").html("");
+        $("#articalContainer").hide();
+    })
+
+    $('#optHint').dblclick(function(){
+        $("#articalMask").hide();
+        $("#articalContainer").hide();
+        $('#optHint').hide();
         $("#articalContainer").html("");
     })
 })
